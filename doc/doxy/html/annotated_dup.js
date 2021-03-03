4c428df2f019f6c6286eec661d76565027bc74a5
Karan Raj Pradhan <karanpradhan96@gmail.com>
2021-03-03 11:31:08 +0530

    updated doxy

--- doc/doxy/html/annotated_dup.js
+++ doc/doxy/html/annotated_dup.js
@@ -1,8 +1,4 @@
 var annotated_dup =
 [
-    [ "This library provides higher level wrapper APIs around the functions provided by the Ai-Thinker SDK.", "md_a9_mqtt_lib__r_e_a_d_m_e.html#autotoc_md11", null ],
-    [ "Functions of this test example:", "md_a9_mqtt_lib__r_e_a_d_m_e.html#autotoc_md12", null ],
-    [ "This library provides functions with better relatable names in order to provide more simplicity and homogeneity while writing code.", "md_a9_power_lib__r_e_a_d_m_e.html#autotoc_md15", null ],
-    [ "Functions of this test example:", "md_a9_power_lib__r_e_a_d_m_e.html#autotoc_md16", null ],
     [ "MQTT_Event_t", "struct_m_q_t_t___event__t.html", "struct_m_q_t_t___event__t" ]
 ];
--- doc/doxy/html/navtreedata.js
+++ doc/doxy/html/navtreedata.js
@@ -27,32 +27,33 @@ var NAVTREE =
   [ "A9/A9G Libraries using Ai-Thinker GPRS C SDK  0.1.0", "index.html", [
     [ "Ai-Thinker GPRS modules- A9/A9G Libraries using [Ai-Thinker GPRS C SDK]", "index.html", [
       [ "Overview", "index.html#autotoc_md22", null ],
-      [ "Hardware", "index.html#autotoc_md23", [
-        [ "1. A9: GPRS Module", "index.html#autotoc_md24", null ],
-        [ "2. A9G: GPRS + GPS + BDS Module", "index.html#autotoc_md25", null ],
-        [ "3. A9", "index.html#autotoc_md26", null ],
-        [ "4. USB to Serial Module", "index.html#autotoc_md27", null ],
-        [ "5. Power Supply", "index.html#autotoc_md28", null ]
+      [ "Features", "index.html#autotoc_md23", null ],
+      [ "Hardware", "index.html#autotoc_md24", [
+        [ "1. A9: GPRS Module", "index.html#autotoc_md25", null ],
+        [ "2. A9G: GPRS + GPS + BDS Module", "index.html#autotoc_md26", null ],
+        [ "3. A9", "index.html#autotoc_md27", null ],
+        [ "4. USB to Serial Module", "index.html#autotoc_md28", null ],
+        [ "5. Power Supply", "index.html#autotoc_md29", null ]
       ] ],
-      [ "Hardware connection", "index.html#autotoc_md29", [
-        [ "Module Debugging and Firmware Download", "index.html#autotoc_md30", null ],
-        [ "Power Supply", "index.html#autotoc_md31", null ]
+      [ "Hardware connection", "index.html#autotoc_md30", [
+        [ "Module Debugging and Firmware Download", "index.html#autotoc_md31", null ],
+        [ "Power Supply", "index.html#autotoc_md32", null ]
       ] ],
-      [ "Setting up the build environment", "index.html#autotoc_md32", [
-        [ "Windows", "index.html#autotoc_md33", [
-          [ "Download the SDK:", "index.html#autotoc_md34", null ],
-          [ "Download and install the toolchain:", "index.html#autotoc_md35", null ],
-          [ "Compile a demo app:", "index.html#autotoc_md36", null ]
+      [ "Setting up the build environment", "index.html#autotoc_md33", [
+        [ "Windows", "index.html#autotoc_md34", [
+          [ "Download the SDK:", "index.html#autotoc_md35", null ],
+          [ "Download and install the toolchain:", "index.html#autotoc_md36", null ],
+          [ "Compile a demo app:", "index.html#autotoc_md37", null ]
         ] ],
-        [ "Linux", "index.html#autotoc_md37", [
-          [ "Download the SDK:", "index.html#autotoc_md38", null ],
-          [ "Download and install the toolchain:", "index.html#autotoc_md39", null ],
-          [ "Compile a demo app:", "index.html#autotoc_md40", null ]
+        [ "Linux", "index.html#autotoc_md38", [
+          [ "Download the SDK:", "index.html#autotoc_md39", null ],
+          [ "Download and install the toolchain:", "index.html#autotoc_md40", null ],
+          [ "Compile a demo app:", "index.html#autotoc_md41", null ]
         ] ],
-        [ "Flash and Debug:", "index.html#autotoc_md41", null ],
-        [ "Project structure and developing applications", "index.html#autotoc_md42", null ],
-        [ "Feedback", "index.html#autotoc_md43", null ],
-        [ "Participate in the development", "index.html#autotoc_md44", null ]
+        [ "Flash and Debug:", "index.html#autotoc_md42", null ],
+        [ "Project structure and developing applications", "index.html#autotoc_md43", null ],
+        [ "Feedback", "index.html#autotoc_md44", null ],
+        [ "Participate in the development", "index.html#autotoc_md45", null ]
       ] ]
     ] ],
     [ "ADS1015 Module for the AiThinker A9(GSM+GPRS) and A9G(GSM+GPRS+GPS)", "md_a9_ads1015_lib__r_e_a_d_m_e.html", [
@@ -104,7 +105,7 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "a9__ads1015__lib_2include_2i2c__lib_8h.html",
-"pages.html"
+"power__example_8c.html#a58fc95dd193ebd2e6c0b221c945d99d9"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
